81802f71c5f97a35f207cad295095cffa9cc0972
derahh <derah666@gmail.com>
2019-04-02 09:50:57 +0700

    change output into sum of fibonancci

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -7,20 +7,21 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scan = new Scanner(System.in);
-        int hasil = 1;
+        int hasil;
         int temp1 = 1;
         int temp2 = 1;
+        int jumlah = temp1 + temp2;
 
         System.out.println("Input:");
         int input = scan.nextInt();
 
         System.out.println("\nOutput:");
-        System.out.print(temp1 + " " + temp2);
         for (int i = 2; i < input; ++i) {
             hasil = temp1 + temp2;
-            System.out.print(" " + hasil);
             temp1 = temp2;
             temp2 = hasil;
+            jumlah += hasil;
         }
+        System.out.println(jumlah);
     }
 }
